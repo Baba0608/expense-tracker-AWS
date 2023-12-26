f1c4a6600db7f68d199a19c84e4310fa0bec8e19
Baba0608 <babafakruddinnitturu@gmail.com>
2023-12-26 09:08:11 +0530

    Removed log statements

--- utils/database.js
+++ utils/database.js
@@ -6,9 +6,6 @@ const USER = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 
-console.log(DB_HOST);
-console.log(PASSWORD);
-
 const sequelize = new Sequelize(DATABASE, USER, PASSWORD, {
   dialect: "mysql",
   host: DB_HOST,
